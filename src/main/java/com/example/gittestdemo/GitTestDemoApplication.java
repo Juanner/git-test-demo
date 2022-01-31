7ede286874aa2e10d04170890a85177fe3e80cbb
Juanner <29154571@qq.com>
2022-01-31 15:00:45 +0800

    github test

--- src/main/java/com/example/gittestdemo/GitTestDemoApplication.java
+++ src/main/java/com/example/gittestdemo/GitTestDemoApplication.java
@@ -13,7 +13,7 @@ public class GitTestDemoApplication {
         System.out.println(i + 1);
         System.out.println(i + 2);
         System.out.println(i + "fenzhi1 ");
-
+        System.out.println(i+"github");
     }
 
 
